a8ea7824c045dd96147478cdb7944adc98b78887
valcucinebrasil <102249283+valcucinebrasil@users.noreply.github.com>
2023-05-30 09:24:50 -0300

    update

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,14 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "A MORTE DO DEMÔNIO - A ASCENSÃO",
+    "description": "A Morte do Demônio: A Ascensão é do mesmo produtor dos sucessos O Homem nas Trevas (2016), Poltergeist - O Fenômeno (2015) e Possessão (2012). Ellie (Alyssa Sutherland) vive com seus filhos Danny (Morgan Davies), Bridget (Gabrielle Echols) e a caçula Kassie num pequeno apartamento, em Los Angeles, California. Para sua surpresa, sua irmã Beth (Lily Sullivan), que andava distante, foi visitá-los, mas o que poderia ser motivo de muitas recordações acaba se transformando num terrível pesadelo. Afinal, o Livro dos Mortos foi reencontrado e com ele novos demônios foram evocados. Agora, Ellie está possuída e as entidades malignas só pensam numa maneira mais sanguinária de destruir essa família.",
+    "photoCover":"https://www.mortedodemonio.com.br/images/share.jpg"
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "title": "Nova saga de Velozes e furiosos",
+    "description": "Velozes e Furiosos 10 é do mesmo diretor do sucesso Carga Explosiva (2002). Ao longo do tempo, Dominic Toretto (Vin Diesel) enfrentou, ao lado da sua família de amigos, os desafios mais radicais de sua vida. Agora, além da volta de Cipher (Charlize Theron), um novo fantasma do passado, Dante (Jason Momoa), está sedento por vingança e quer eliminar todos que Dom mais ama, como seu pequeno filho. Chegou a hora da insana equipe de pilotos, Jakob (John Cena), Han (Sung Kang), Mia (Jordana Brewster), Roman (Tyrese Gibson), Tej (Ludacris), apertar os cintos e encarar sua missão mais mortal. Do mesmo produtor de todos os filmes da série, de Velozes e Furiosos (2001) até Velozes & Furiosos 9 (2021).",
+    "photoCover":"https://s2.glbimg.com/ZE5QdzDooRk63TTfXLCFVz40Q0g=/1200x/smart/filters:cover():strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/G/N/HbXPRMTOmB2h5MbBeTWQ/velozes-furiosos-justwatch-techtudo.jpg"
   }
 ]
