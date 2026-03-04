a213cdf38cafc3c99cc84df0cc5154151e41ca5c
Zoé Mateus <83597652+Zoemateus324@users.noreply.github.com>
2026-03-04 09:20:10 -0300

    backup

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -10,5 +10,15 @@ export const dataFake = [
     "title": "Nova saga de Velozes e furiosos",
     "description": "Velozes e Furiosos 10 é do mesmo diretor do sucesso Carga Explosiva (2002). Ao longo do tempo, Dominic Toretto (Vin Diesel) enfrentou, ao lado da sua família de amigos, os desafios mais radicais de sua vida. Agora, além da volta de Cipher (Charlize Theron), um novo fantasma do passado, Dante (Jason Momoa), está sedento por vingança e quer eliminar todos que Dom mais ama, como seu pequeno filho. Chegou a hora da insana equipe de pilotos, Jakob (John Cena), Han (Sung Kang), Mia (Jordana Brewster), Roman (Tyrese Gibson), Tej (Ludacris), apertar os cintos e encarar sua missão mais mortal. Do mesmo produtor de todos os filmes da série, de Velozes e Furiosos (2001) até Velozes & Furiosos 9 (2021).",
     "photoCover":"https://s2.glbimg.com/ZE5QdzDooRk63TTfXLCFVz40Q0g=/1200x/smart/filters:cover():strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/G/N/HbXPRMTOmB2h5MbBeTWQ/velozes-furiosos-justwatch-techtudo.jpg"
+  },
+  {"id":"3",
+    "title": "O Homem do Norte",
+    "description": "O Homem do Norte é do mesmo diretor do sucesso A Bruxa (2015). No século X, o príncipe viking Amleth (Alexander Skarsgård) vê seu pai ser assassinado por seu tio, Fjölnir (Claes Bang), que também sequestra sua mãe. Amleth consegue escapar e jura vingança. Anos depois, ele retorna para a Dinamarca para enfrentar Fjölnir e resgatar sua mãe.",
+    "photoCover":"https://www.opovo.com.br/_midias/jpg/2022/10/11/750x500/1_2022101110545679.jpg"
+  },
+  {"id":"4",
+    "title": "O Gato de Botas 2: O Último Pedido",
+    "description": "O Gato de Botas 2: O Último Pedido é do mesmo diretor do sucesso Shrek (2001). O Gato de Botas (Antonio Banderas) descobre que sua paixão por aventuras e sua despreocupação pela segurança pessoal têm cobrado um preço: ele já usou oito de suas nove vidas. Determinado a recuperar suas vidas perdidas, o Gato parte em uma jornada épica para encontrar a mítica Estrela dos Desejos e restaurar suas vidas antes que seja tarde demais.",
+    "photoCover":"https://www.cineclick.com.br/uploads/filmes/2022/11/08/filme-o-gato-de-botas-2.jpg"
   }
 ]
